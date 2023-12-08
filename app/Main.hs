b3ea13529fc3b81b62fd7855bf9c8f4348d78ce0
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2023-12-08 16:56:18 +0100

    Recreate database

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,14 @@ module Main where
 import Configuration.Dotenv (loadFile, defaultConfig, onMissingFile)
 import Control.Lens ( (&), (.~) )
 import Data.Text (pack)
-import Database.InfluxDB (writeBatch, writeParams)
+import Database.InfluxDB
 import Exporter (toMeasurements)
 import Hledger.Data.Types (jtxns)
 import Hledger.Read (defaultJournal)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import System.Environment.Blank (getEnvDefault, getEnvironment)
 import qualified Database.InfluxDB.Types as I
+import qualified Database.InfluxDB.Format as F
 
 main :: IO ()
 main = do
@@ -25,8 +26,15 @@ main = do
   let measurements = toMeasurements (jtxns journal)
   let db = I.Database (pack dbName)
   let server = I.Server (pack host) (read port :: Int) (read ssl :: Bool)
-  let params = writeParams db & I.server .~ server
-                              & I.manager .~ Left tlsManagerSettings
 
-  writeBatch params measurements
-  putStrLn $ "Wrote " ++ show (length measurements) ++ " measurements."
+  let qp = queryParams db & I.server .~ server
+                          & I.manager .~ Left tlsManagerSettings
+  manage qp $ formatQuery ("DROP DATABASE "%F.database) db
+  manage qp $ formatQuery ("CREATE DATABASE "%F.database) db
+  putStrLn $ "Recreated database " ++ dbName
+
+  let wp = writeParams db & I.server .~ server
+                          & I.manager .~ Left tlsManagerSettings
+
+  writeBatch wp measurements
+  putStrLn $ "Wrote " ++ show (length measurements) ++ " measurements"
